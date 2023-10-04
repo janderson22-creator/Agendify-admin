93a18ec70cf4f7168c8a4582dd9f856d1ca28234
Janderson Costa <jandersomcosta@yahoo.com.br>
2023-10-04 15:32:21 -0300

    react big calendar

--- src/pages/Schedules/index.tsx
+++ src/pages/Schedules/index.tsx
@@ -1,150 +1,36 @@
-import { useEffect, useMemo, useState } from "react";
-import CalendarIcon from "../../assets/icons/calendar-icon.svg";
-import { useCommerce } from "../../context/commerce";
-import DatePicker from "../../components/CalendarInput";
-import Employees from "../../components/Empoyees";
-import classNames from "../../utils/className";
-import Tooltip from "../../components/Base/tooltip";
-import InputSearch from "../../components/Base/input-search";
-import ToSchedule from "../../components/ToSchedule";
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import "moment/locale/pt-br";
+const localizer = momentLocalizer(moment);
+
+// Simulação de dados de agendamento
+const eventos = [
+  {
+    title: "Reunião com Cliente 1",
+    start: new Date(2023, 9, 10, 9, 0), // Ano, Mês (0-11), Dia, Hora, Minuto
+    end: new Date(2023, 9, 10, 10, 0),
+  },
+  {
+    title: "Reunião com Cliente 2",
+    start: new Date(2023, 9, 11, 14, 0),
+    end: new Date(2023, 9, 11, 15, 0),
+  },
+  // Adicione mais eventos conforme necessário
+];
 
 const Schedules: React.FC = () => {
-  const {
-    fetchEstablishmentsById,
-    currentCommerce,
-    formattedDate,
-    setFormattedDate,
-  } = useCommerce();
-  const [hoverTooltip, setHoverTooltip] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
-  const [schedulesEmployee, setSchedulesEmployee] = useState<string[]>([]);
-
-  useEffect(() => {
-    if (currentCommerce) return;
-
-    const id = location.pathname.split("/")[2];
-
-    fetchEstablishmentsById(id);
-  }, []);
-
-  useEffect(() => {
-    setFormattedDate({
-      dayOnWeek: "",
-      month: "",
-      day: "",
-      year: "",
-      name_employee: "",
-      name_user: "",
-      description: "",
-      phone_number: "",
-      service: "SERVIÇO",
-      time: "HORÁRIO",
-    });
-  }, []);
-
-  const dateSelected = useMemo(() => {
-    if (
-      !formattedDate.day ||
-      !formattedDate.dayOnWeek ||
-      !formattedDate.month ||
-      !formattedDate.year
-    ) {
-      return false;
-    } else {
-      return true;
-    }
-  }, [formattedDate]);
-
   return (
-    <div className="w-full">
-      <div className="flex flex-col h-fit w-full rounded-[10px] py-5 px-4 mt-[30px]">
-        <div>
-          <label className="text-[#5C6666] text-[18px] font-semibold mr-4">
-            Selecione a data que deseja o serviço:
-          </label>
-          <DatePicker />
-        </div>
-
-        <div
-          onMouseEnter={() => setHoverTooltip(true)}
-          onMouseLeave={() => setHoverTooltip(false)}
-          className="relative w-full mt-5"
-        >
-          <div
-            className={classNames(
-              "w-full",
-              dateSelected
-                ? "opacity-100"
-                : "opacity-20 pointer-events-none cursor-not-allowed"
-            )}
-          >
-            <InputSearch
-              value={searchValue}
-              setValue={setSearchValue}
-              placeholder="Buscar profissional"
-            />
-            <Employees
-              value={searchValue}
-              setSchedules={setSchedulesEmployee}
-            />
-          </div>
-
-          {hoverTooltip && !dateSelected && (
-            <Tooltip message={"Selecione a data que deseja o atendimento"} />
-          )}
-
-          {hoverTooltip &&
-            dateSelected &&
-            !formattedDate.name_employee &&
-            searchValue === "" && (
-              <Tooltip
-                message={"Selecione o profissinal que deseja o atendimento"}
-              />
-            )}
-        </div>
-
-        <div
-          className={classNames(
-            "relative flex items-center mt-10",
-            formattedDate?.name_employee
-              ? "opacity-100"
-              : "opacity-20 pointer-events-none cursor-not-allowed"
-          )}
-        >
-          <div className="flex items-center justify-center w-[52px] h-[52px] bg-[#F0F0F5] rounded-[8px]">
-            <img
-              src={CalendarIcon}
-              alt="calendar_schedules"
-              className="object-cover"
-            />
-          </div>
-
-          <div className="flex flex-col text-[#5C6666] ml-3">
-            <span className="font-bold">
-              {!formattedDate?.name_employee
-                ? "Selecione a data e o profissional que deseja"
-                : `${formattedDate?.dayOnWeek}
-              em ${formattedDate?.day} de ${formattedDate?.month} de
-              ${formattedDate?.year} com ${formattedDate.name_employee}`}
-            </span>
-            <p className="text-xs mt-2">Data</p>
-          </div>
-        </div>
-
-        <div
-          className={classNames(
-            "mt-14",
-            formattedDate?.name_employee
-              ? "opacity-100"
-              : "opacity-20 pointer-events-none cursor-not-allowed"
-          )}
-        >
-          <ToSchedule
-            services={currentCommerce?.services}
-            schedules={schedulesEmployee}
-          />
-        </div>
-      </div>
+    <div>
+      <h2>Calendário de Agendamentos</h2>
+      <Calendar
+        localizer={localizer}
+        events={eventos}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }} // Altura do calendário
+      />
     </div>
   );
 };
